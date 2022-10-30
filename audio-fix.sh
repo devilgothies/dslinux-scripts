526db906e38a2cb58e4a3c23921b493494d4f229
Lukka Wayand <wh1skysec9rity@gmail.com>
2022-10-29 21:04:22 -0300

    [add] less if's statements

--- audio-fix.sh
+++ audio-fix.sh
@@ -21,11 +21,11 @@ MULTILASER_USB_MIC="alsa_input.usb-C-Media_Electronics_Inc._USB_PnP_Sound_Device
 # BUSCANDO MÓDULOS DE ÁUDIO #
 #############################
 GREP_SINK() { # Busca os speakers (fones de ouvido) disponíveis no PulseAudio
-	su manager -s /bin/bash -c "pactl list sinks | grep 'Nome.*PnP' | sed 's/[[:blank:]]//    g' | cut -d ':' -f 2"
+	su manager -s /bin/bash -c "pactl list sinks | grep 'Nome.*PnP' | sed 's/[[:blank:]]//g' | cut -d ':' -f 2"
 }
 
 GREP_SOURCE() { # Busca os microfones disponíveis no PulseAudio
-	su manager -s /bin/bash -c "pactl list sources | grep 'Nome.*PnP.*analog-mono' | sed     's/[[:blank:]]//g' | cut -d ':' -f 2"
+	su manager -s /bin/bash -c "pactl list sources | grep 'Nome.*PnP.*analog-mono' | sed 's/[[:blank:]]//g' | cut -d ':' -f 2"
 }
 
 DELETE_OLD_CONFIG() { # Deleta as configurações default antigas
@@ -46,36 +46,36 @@ then
 	echo
 	echo -e "${Cyan}[*] MÓDULOS ENCONTRADOS: ${ColorOff}"
 	
-  if [[ $(OUT_GS=$(GREP_SINK)) ]]; then # Verifica e informa os speakers reconhecidos
+  	if [[ $(OUT_GS=$(GREP_SINK)) ]]; then # Verifica e informa os speakers reconhecidos
 		echo -e "${Green}Fone: $OUT_GS ${ColorOff}"
 	else
 		echo -e "${Red}[!] Nenhum speaker (fone) encontrado.${ColorOff}"
 	fi
 	
-
-  if [[ $(OUT_GM=$(GREP_SOURCE)) ]]; then # Verifica e informa os microfones reconhecidos
+  	if [[ $(OUT_GM=$(GREP_SOURCE)) ]]; then # Verifica e informa os microfones reconhecidos
 		echo -e "${Green}Microfone: $OUT_GM ${ColorOff}"
 	else
 		echo -e "${Red}[!] Nenhum microfone encontrado.${ColorOff}"
 	fi
-	echo
-	sleep 1
-	echo -e "${Cyan}[*] Apagando configurações anteriores...${ColorOff}"
-	sleep 1
-	sed -i '/set-default-*/d' /etc/pulse/default.pa
-	echo -e "${Cyan}[*] Adicionando configurações novas...${ColorOff}"
-	sleep 1
-	echo "set-default-sink "$OUT_GS >> /etc/pulse/default.pa
-	echo "set-default-source "$OUT_GM >> /etc/pulse/default.pa
 
-	
-	if [[ $? -eq 0 ]]; then # Verifica se as confiugrações novas foram adicionas corretamente
+	if [[ $(OUT_GS=$(GREP_SINK)) ]] && [[ $(OUT_GM=$(GREP_SOURCE)) ]]; then # Verifica se algum módulo foi encontrado, caso contrário, o script é abortado
 		echo
-		echo -e "${Green}[+] Configurações finalizadas.${ColorOff}"
+		sleep 1
+		echo -e "${Cyan}[*] Apagando configurações anteriores...${ColorOff}"
+		sleep 1
+		sed -i '/set-default-*/d' /etc/pulse/default.pa
+		echo -e "${Cyan}[*] Adicionando configurações novas...${ColorOff}"
+		sleep 1
+		echo "set-default-sink "$OUT_GS >> /etc/pulse/default.pa
+		echo "set-default-source "$OUT_GM >> /etc/pulse/default.pa
+		if [[ $? -eq 0 ]]; then # Verifica se as configurações novas foram adicionas corretamente
+			echo
+			echo -e "${Green}[+] Configurações finalizadas.${ColorOff}"
+		fi
 	else
+		echo
 		echo -e "${Red}[!] As configurações não puderam ser finalizadas.${ColorOff}"
 	fi
-	
 else
 	echo -e "${Red}[!] Cancelando...${ColorOff}"
 fi
